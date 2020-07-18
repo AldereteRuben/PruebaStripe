382fb4057d31a2311c67c2cb9a8e6e8b0d78e750
Ruben Alderete <rubenalderete97@gmail.com>
2020-07-18 18:59:19 -0500

    Se resolvio el error de overflow al desplegar el teclado, se agrego la parte de Registrarte abajo de los botones circulares, al igual que se arreglo otros problemas de responsivos de la app en el front de login

--- pruebastripe/lib/pages/login/widgets/login_form.dart
+++ pruebastripe/lib/pages/login/widgets/login_form.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pruebastripe/pages/login/widgets/input_text_login.dart';
 import 'package:pruebastripe/pages/login/widgets/rounded_button.dart';
+import 'package:pruebastripe/utils/responsive.dart';
 import 'package:pruebastripe/widgets/circle_button.dart';
 
 class LoginForm extends StatelessWidget {
@@ -9,64 +10,81 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 330,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          InputTextLogin(
-              iconPath: "assets/pages/login/icons/correo-electronico.svg",
-              placeholder: "Correo Electronico"),
-          SizedBox(
-            height: 10,
-          ),
-          InputTextLogin(
-              iconPath: "assets/pages/login/icons/llave.svg",
-              placeholder: "Contraseña"),
-          Container(
-            width: double.infinity,
-            alignment: Alignment.centerRight,
-            child: CupertinoButton(
-                padding: EdgeInsets.symmetric(vertical: 15),
-                child: Text(
-                  '¿Recuperar contraseña?',
-                  style: TextStyle(fontFamily: 'sans'),
+    final Responsive responsive = Responsive.of(context);
+    return SafeArea(
+      top: false,
+      child: Container(
+        width: 330,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            InputTextLogin(
+                iconPath: "assets/pages/login/icons/correo-electronico.svg",
+                placeholder: "Correo Electronico"),
+            SizedBox(
+              height: responsive.ip(2),
+            ),
+            InputTextLogin(
+                iconPath: "assets/pages/login/icons/llave.svg",
+                placeholder: "Contraseña"),
+            Container(
+              width: double.infinity,
+              alignment: Alignment.centerRight,
+              child: CupertinoButton(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Text(
+                    '¿Recuperar contraseña?',
+                    style: TextStyle(fontFamily: 'sans'),
+                  ),
+                  onPressed: () {}),
+            ),
+            SizedBox(
+              height: responsive.ip(2),
+            ),
+            RoudedButton(onPressed: () {}, label: "Entrar"),
+            SizedBox(
+              height: responsive.ip(3.3),
+            ),
+            Text("O continuar con"),
+            SizedBox(
+              height: responsive.ip(1),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleButton(
+                  size: 55,
+                  iconPath: 'assets/pages/login/icons/facebook.svg',
+                  backgroundColor: Color(0xff1448AFF),
                 ),
-                onPressed: () {}),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          RoudedButton(onPressed: () {}, label: "Entrar"),
-          SizedBox(
-            height: 30,
-          ),
-          Text("O continuar con"),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleButton(
-                size: 55,
-                iconPath: 'assets/pages/login/icons/facebook.svg',
-                backgroundColor: Color(0xff1448AFF),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              CircleButton(
-                backgroundColor: Color(0xffFF1744),
-                iconPath: 'assets/pages/login/icons/google.svg',
-                size: 55,
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 60,
-          )
-        ],
+                SizedBox(
+                  width: 20,
+                ),
+                CircleButton(
+                  backgroundColor: Color(0xffFF1744),
+                  iconPath: 'assets/pages/login/icons/google.svg',
+                  size: 55,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: responsive.ip(2.7),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("¿No tienes una cuenta?"),
+                CupertinoButton(
+                    child: Text(
+                      'Registrate',
+                      style: TextStyle(
+                          fontFamily: 'sans', fontWeight: FontWeight.w600),
+                    ),
+                    onPressed: () {})
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
