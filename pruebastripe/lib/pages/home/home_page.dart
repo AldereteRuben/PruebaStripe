f309eb51fe2062a54d7382652145d1ef53c612e7
Ruben Alderete <rubenalderete97@gmail.com>
2020-07-21 14:25:53 -0500

    Se agrego en la vista home_page que cuando ingresas con FB se muestre la foto de perfil, nombre , correo, fecha en que se registro en la app y la ultima conexón

--- pruebastripe/lib/pages/home/home_page.dart
+++ pruebastripe/lib/pages/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pruebastripe/libs/auth.dart';
 
@@ -11,15 +13,93 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget _getAlias(String displayName) {
+    final List<String> tmp = displayName.split(" ");
+    String alias = "";
+    if (tmp.length > 0) {
+      alias = tmp[0][0];
+      if (tmp.length == 2) {
+        alias += tmp[1][0];
+      }
+    }
+    return Center(
+      child: Text(
+        alias,
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-            onPressed: () {
-              Auth.instance.logOut(context);
-            },
-            child: Text('Cerrar Sesión')),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: SafeArea(
+          child: FutureBuilder<FirebaseUser>(
+              future: Auth.instance.user,
+              builder: (BuildContext _, AsyncSnapshot<FirebaseUser> snapshot) {
+                if (snapshot.hasData) {
+                  final user = snapshot.data;
+
+                  return ListView(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CircleAvatar(
+                          radius: 40,
+                          child: user.photoUrl != null
+                              ? ClipOval(
+                                  child: Image.network(
+                                    user.photoUrl,
+                                    width: 75,
+                                    height: 75,
+                                    fit: BoxFit.contain,
+                                  ),
+                                )
+                              : _getAlias(user.displayName)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        user.displayName,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        user.email,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w300),
+                      ),
+                      Text(
+                        user.metadata.creationTime.toString(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w300),
+                      ),
+                      Text(
+                        user.metadata.lastSignInTime.toString(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Error de conexión"),
+                  );
+                }
+                return Center(
+                  child: CupertinoActivityIndicator(),
+                );
+              }),
+        ),
       ),
     );
   }
