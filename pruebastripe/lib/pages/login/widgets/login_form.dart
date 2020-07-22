2b3f0f3d65c2d7621e155d935d3910c21dd005dd
Anna <analaurapco@gmail.com>
2020-07-22 15:45:19 -0500

    Se modifico una condiion

--- pruebastripe/lib/pages/login/widgets/login_form.dart
+++ pruebastripe/lib/pages/login/widgets/login_form.dart
@@ -36,7 +36,7 @@ class _LoginFormState extends State<LoginForm> {
     }
   }
 
-  Future<void> _submit() async {
+  _submit() async {
     final String email = _emailKey.currentState.value;
     final String password = _passwordKey.currentState.value;
 
